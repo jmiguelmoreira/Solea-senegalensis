5cdb79c8386ba5009b55d5b5d50fef546c58e563
jmiguelmoreira <83463634+jmiguelmoreira@users.noreply.github.com>
2021-06-09 00:12:16 +0100

    Add files via upload

--- pars_init_Solea_senegalensis.m
+++ pars_init_Solea_senegalensis.m
@@ -6,32 +6,45 @@ metaPar.model = 'abj';
 par.T_ref = 293.15;   free.T_ref = 0;   units.T_ref = 'K';        label.T_ref = 'Reference temperature'; 
 
 %% core primary parameters 
-par.T_A = 6114;       free.T_A   = 0;   units.T_A = 'K';          label.T_A = 'Arrhenius temperature'; 
-par.z = 3.9093;       free.z     = 1;   units.z = '-';            label.z = 'zoom factor'; 
+par.T_A = 6395.8756;  free.T_A   = 1;   units.T_A = 'K';          label.T_A = 'Arrhenius temperature'; 
+par.z = 4.6415;       free.z     = 1;   units.z = '-';            label.z = 'zoom factor'; 
 par.F_m = 6.5;        free.F_m   = 0;   units.F_m = 'l/d.cm^2';   label.F_m = '{F_m}, max spec searching rate'; 
 par.kap_X = 0.8;      free.kap_X = 0;   units.kap_X = '-';        label.kap_X = 'digestion efficiency of food to reserve'; 
 par.kap_P = 0.1;      free.kap_P = 0;   units.kap_P = '-';        label.kap_P = 'faecation efficiency of food to faeces'; 
-par.v = 0.094544;     free.v     = 1;   units.v = 'cm/d';         label.v = 'energy conductance'; 
-par.kap = 0.72666;    free.kap   = 1;   units.kap = '-';          label.kap = 'allocation fraction to soma'; 
+par.v = 0.092561;     free.v     = 1;   units.v = 'cm/d';         label.v = 'energy conductance'; 
+par.kap = 0.78269;    free.kap   = 1;   units.kap = '-';          label.kap = 'allocation fraction to soma'; 
 par.kap_R = 0.95;     free.kap_R = 0;   units.kap_R = '-';        label.kap_R = 'reproduction efficiency'; 
-par.p_M = 15.0447;    free.p_M   = 1;   units.p_M = 'J/d.cm^3';   label.p_M = '[p_M], vol-spec somatic maint'; 
+par.p_M = 17.9699;    free.p_M   = 1;   units.p_M = 'J/d.cm^3';   label.p_M = '[p_M], vol-spec somatic maint'; 
 par.p_T = 0;          free.p_T   = 0;   units.p_T = 'J/d.cm^2';   label.p_T = '{p_T}, surf-spec somatic maint'; 
 par.k_J = 0.002;      free.k_J   = 0;   units.k_J = '1/d';        label.k_J = 'maturity maint rate coefficient'; 
-par.E_G = 5235.2009;  free.E_G   = 1;   units.E_G = 'J/cm^3';     label.E_G = '[E_G], spec cost for structure'; 
-par.E_Hb = 3.650e-01; free.E_Hb  = 1;   units.E_Hb = 'J';         label.E_Hb = 'maturity at birth'; 
-par.E_Hj = 1.010e+01; free.E_Hj  = 1;   units.E_Hj = 'J';         label.E_Hj = 'maturity at metam'; 
-par.E_Hp = 1.020e+06; free.E_Hp  = 1;   units.E_Hp = 'J';         label.E_Hp = 'maturity at puberty'; 
-par.h_a = 9.318e-09;  free.h_a   = 0;   units.h_a = '1/d^2';      label.h_a = 'Weibull aging acceleration'; 
-par.s_G = 0.0001;     free.s_G   = 0;   units.s_G = '-';          label.s_G = 'Gompertz stress coefficient'; 
+par.E_G = 5297.5665;  free.E_G   = 1;   units.E_G = 'J/cm^3';     label.E_G = '[E_G], spec cost for structure'; 
+par.E_Hb = 2.604e-01; free.E_Hb  = 1;   units.E_Hb = 'J';         label.E_Hb = 'maturity at birth'; 
+par.E_Hj = 7.209e+00; free.E_Hj  = 1;   units.E_Hj = 'J';         label.E_Hj = 'maturity at metamorphosis'; 
+par.E_Hp = 9.384e+05; free.E_Hp  = 1;   units.E_Hp = 'J';         label.E_Hp = 'maturity at puberty'; 
+par.h_a = 5.629e-09;  free.h_a   = 1;   units.h_a = '1/d^2';      label.h_a = 'Weibull aging acceleration'; 
+par.s_G = 0.001;      free.s_G   = 0;   units.s_G = '-';          label.s_G = 'Gompertz stress coefficient'; 
 
 %% other parameters 
-par.E_Hh = 2.199e-01; free.E_Hh  = 1;   units.E_Hh = 'J';         label.E_Hh = 'maturity at hatch'; 
-par.del_M = 0.24141;  free.del_M = 1;   units.del_M = '-';        label.del_M = 'shape coefficient'; 
-par.f = 1;            free.f     = 0;   units.f = '-';            label.f = 'scaled functional response for 0-var data'; 
-par.f_34 = 1;         free.f_34  = 1;   units.f_34 = '-';         label.f_34 = 'scaled functional response for 3&4 data'; 
+par.E_Hh = 1.583e-01; free.E_Hh  = 1;   units.E_Hh = 'J';         label.E_Hh = 'maturity at hatch'; 
+par.T_AH = 100000;    free.T_AH  = 0;   units.T_AH = 'K';         label.T_AH = 'T_AH'; 
+par.T_AL = 50000;     free.T_AL  = 0;   units.T_AL = 'K';         label.T_AL = 'T_AL'; 
+par.T_H = 303;        free.T_H   = 0;   units.T_H = 'K';          label.T_H = 'T_H'; 
+par.T_L = 285;        free.T_L   = 0;   units.T_L = 'K';          label.T_L = 'T_L'; 
+par.del_M = 0.24184;  free.del_M = 1;   units.del_M = '-';        label.del_M = 'del_M, shape coefficient'; 
+par.del_Me = 0.24843;  free.del_Me = 1;   units.del_Me = '-';       label.del_Me = 'shape coefficient for embryo'; 
+par.f = 0.83699;      free.f     = 1;   units.f = '-';            label.f = 'scaled functional response for YufeParr1999 data'; 
 par.f_CB = 1;         free.f_CB  = 1;   units.f_CB = '-';         label.f_CB = 'scaled functional response for Castelo Branco data'; 
+par.f_CanaFern1 = 0.42751;  free.f_CanaFern1 = 1;   units.f_CanaFern1 = '-';  label.f_CanaFern1 = 'scaled functional response for tWd_f1 data'; 
+par.f_CanaFern2 = 0.47234;  free.f_CanaFern2 = 1;   units.f_CanaFern2 = '-';  label.f_CanaFern2 = 'scaled functional response for tWd_f2 data'; 
+par.f_CanaFern3 = 0.57401;  free.f_CanaFern3 = 1;   units.f_CanaFern3 = '-';  label.f_CanaFern3 = 'scaled functional response for tWd_f3 data'; 
+par.f_CanaFern4 = 0.50361;  free.f_CanaFern4 = 1;   units.f_CanaFern4 = '-';  label.f_CanaFern4 = 'scaled functional response for tWd_f3 data'; 
 par.f_E = 1;          free.f_E   = 1;   units.f_E = '-';          label.f_E = 'scaled functional response for AM data'; 
+par.f_TeixCabr = 0.7544;  free.f_TeixCabr = 1;   units.f_TeixCabr = '-';   label.f_TeixCabr = 'scaled functional response for tL3 data'; 
+par.f_TeixCabr2 = 0.66015;  free.f_TeixCabr2 = 1;   units.f_TeixCabr2 = '-';  label.f_TeixCabr2 = 'scaled functional response for Tl4 data'; 
 par.f_exp = 1;        free.f_exp = 1;   units.f_exp = '-';        label.f_exp = 'scaled functional response for PA exp data'; 
+par.f_field = 0.99999;  free.f_field = 1;   units.f_field = '-';      label.f_field = 'scaled functional response for 0-var data fromthe field'; 
+par.f_tL = 0.99593;   free.f_tL  = 1;   units.f_tL = '-';         label.f_tL = 'scaled functional response for tL data'; 
+par.f_tL2 = 0.51807;  free.f_tL2 = 1;   units.f_tL2 = '-';        label.f_tL2 = 'scaled functional response for tL2 data'; 
 par.s_pH = 0;         free.s_pH  = 0;   units.s_pH = '-';         label.s_pH = 'stress factor due to pH level'; 
 
 %% set chemical parameters from Kooy2010 
--- predict_Solea_senegalensis.m
+++ predict_Solea_senegalensis.m
@@ -20,9 +20,9 @@ if f_E > 1
     prdData = []; info = 0; return
 end
 
-if f_34 > 1
-    prdData = []; info = 0; return
-end
+% if f_34 > 1
+%     prdData = []; info = 0; return
+% end
 
 % if s_pH < 0
 %     prdData = []; info = 0; return
@@ -31,6 +31,15 @@ end
 if E_Hh >= E_Hb
     prdData = []; info = 0; return
 end
+
+ filterChecks =   E_Hh > E_Hb || E_Hh <= 0 || f_field > 1 || f_tL > 1 || f_tL2 > 1 || ... % maturity at hatching has to be between 0 and Ehb
+     f_tL < 0.1 || f_tL2 < 0.1 || ...
+     ~reach_birth(g, k, v_Hb, f_tL) || ... % constraint required for reaching birth with that f
+     ~reach_birth(g, k, v_Hb, f_tL2) ; %|| ...  % constraint required for reaching birth with that f
+if filterChecks  
+    info = 0;
+    prdData = {}; return
+end 
         
   % compute temperature correction factors
   TC_ah = tempcorr(temp.ah, T_ref, T_A);
@@ -38,14 +47,27 @@ end
   TC_aj = tempcorr(temp.aj, T_ref, T_A);
   TC_ap = tempcorr(temp.ap, T_ref, T_A);
   TC_am = tempcorr(temp.am, T_ref, T_A);
+  TC_tj = tempcorr(temp.tj, T_ref, T_A);
   TC_Ri = tempcorr(temp.Ri, T_ref, T_A);
   %TC_Teal = tempcorr(temp.tLTeal, T_ref, T_A);
-  TC_CB = tempcorr(temp.tWwCB, T_ref, T_A);
+  TC_CB = tempcorr(temp.tLCB, T_ref, T_A);
   TC_A = tempcorr(temp.tWwA, T_ref, T_A);
   TC_B = tempcorr(temp.tWwB, T_ref, T_A);
   TC_C = tempcorr(temp.tWwC, T_ref, T_A);
   TC_D = tempcorr(temp.tWwD, T_ref, T_A);
   TC_E = tempcorr(temp.tWwE, T_ref, T_A);
+  
+    T_pars=[T_A, T_L, T_H, T_AL, T_AH];
+  TC_tL = tempcorr(temp.tL1, T_ref, T_pars);
+  TC_tL2 = tempcorr(temp.tL2, T_ref, T_pars);
+  TC_tL3 = tempcorr(temp.tL3, T_ref, T_pars);
+  TC_tL4 = tempcorr(temp.tL4, T_ref, T_pars);
+  TC_tWd = tempcorr(temp.tWd(1), T_ref, T_pars);
+  TC_tWd2 = tempcorr(temp.tWd2(1), T_ref, T_pars);
+  TC_tWd_f1 = tempcorr(temp.tWd_f1(1), T_ref, T_pars);
+  TC_tWd_f2 = tempcorr(temp.tWd_f2(1), T_ref, T_pars);
+  TC_tWd_f3 = tempcorr(temp.tWd_f3(1), T_ref, T_pars);
+  TC_tWd_f4 = tempcorr(temp.tWd_f4(1), T_ref, T_pars);
   TC_3 = tempcorr(temp.tL3, T_ref, T_A);
   TC_4 = tempcorr(temp.tL4, T_ref, T_A);
   
@@ -59,30 +81,37 @@ end
      % initial
   pars_UE0 = [V_Hb; g; k_J; k_M; v]; % compose parameter vector
   U_E0 = initial_scaled_reserve(f, pars_UE0); % d.cm^2, initial scaled reserve
+  E_0 = U_E0 * p_Am ;          % J, energy in egg
+  Wd_0 = E_0 * w_E/ mu_E;      % g, egg dry weight 
   
     % hatch   
   [U_H aUL] = ode45(@dget_aul, [0; U_Hh; U_Hb], [0 U_E0 1e-10], [], kap, v, k_J, g, L_m);
   aT_h = aUL(2,1)/ TC_ah;          % d, age at hatch at f and T
   M_Eh = J_E_Am * aUL(2,2);        % mol, reserve at hatch at f
   L_h = aUL(2,3);                  % cm, structural length at f
-  Lw_h = L_h/ del_M;               % cm, S-V length at hatch at f
+  Lw_h = L_h/ del_Me;               % cm, S-V length at hatch at f
   Ww_h = L_h^3 * (1 + f * w);
-  Wd_h = Ww_h * d_V;
+%   Wd_h = Ww_h * d_V;
+  E_h = aUL(2,2) * p_Am *TC_ah;             % J, energy in reserves at hatch
+  Wd_h = (d_V * L_h^3 + w_E/ mu_E * E_h); % ug, dry weight at hatch; 
   
   % birth
   L_b = L_m * l_b;                  % cm, structural length at birth at f
-  Lw_b = L_b/ del_M;                % cm, total length at birth at f
+  Lw_b = L_b/ del_Me;                % cm, total length at birth at f
   Ww_b = L_b^3 * (1 + f * w);       % g, wet weight at birth at f 
-  Wd_b = Ww_b * d_V;
+%   Wd_b = Ww_b * d_V;
+  Wd_b = d_V *L_b^3 * (1 + f * w); % ug, dry weight at birth at f 
   a_b = t_b/ k_M; aT_b = a_b/ TC_ab; % d, age at birth at f and T
 
   % metam
   L_j = L_m * l_j;                  % cm, structural length at birth at f
-  Lw_j = L_j/ del_M;                % cm, total length at birth at f
+  Lw_j = L_j/ del_Me;                % cm, total length at birth at f
   Ww_j = L_j^3 * (1 + f * w);
-  Wd_j = Ww_j * d_V;
+%   Wd_j = Ww_j * d_V;
   %Wd_j = L_j^3 * d_V + E_j * w_E/ mu_E; % g, ultimate dry weight (remove d_V for wet weight)
+  Wd_j = L_j^3 * d_V * (1 + f * w); % ug, dry weight at metam 
   aT_j = t_j/ k_M/ TC_aj;   % d, age at birth at metam
+  tT_j = (t_j - t_b) / k_M/ TC_tj;  % d, time since birth at metam
 
   % puberty 
   L_p = L_m * l_p;                  % cm, structural length at puberty at f
@@ -110,16 +139,20 @@ end
   prdData.aj = aT_j;
   prdData.ap = aT_p;
   prdData.am = aT_m;
+  prdData.tj = tT_j;
   prdData.Lh = Lw_h;
   prdData.Lb = Lw_b;
   prdData.Lj = Lw_j;
-  prdData.Lp = Lw_p;
+  prdData.Lp_f = Lw_p;  prdData.Lp_m = Lw_p;
   prdData.Li = Lw_i;
+  prdData.Wd0 = Wd_0;
   prdData.Wdh = Wd_h;
   prdData.Wdb = Wd_b;
   prdData.Wdj = Wd_j;
+  prdData.Wwp_f = Ww_p;prdData.Wwp_m = Ww_p;
   prdData.Wwi = Ww_i;
   prdData.Ri = RT_i;
+  prdData.E0 = E_0;
   
   % uni-variate data
   
@@ -132,23 +165,32 @@ end
 %   L_jm = L_i - (L_i - L_j) * exp( - rT_B * (tLTeal((tLTeal(:,1) > tT_j),1) - tT_j)); % cm, struct length
 %   ELw = [L_bj; L_jm]/ del_M;  % cm, total length
   
-    %time-length 3
-  [t_j, t_p, t_b, l_j, l_p, l_b, l_i, rho_j, rho_B] = get_tj(pars_tj, f_34);
-  kT_M = k_M * TC_3; rT_j = rho_j * kT_M; rT_B = rho_B * kT_M;        
-  L_b = L_m * l_b;  L_j = L_m * l_j; L_i = L_m * l_i; tT_j = (t_j - t_b)/ kT_M;
-  L_bj = L_b * exp(tL3((tL3(:,1) <= tT_j),1) * rT_j/ 3); % cm, struc length
-  L_jm = L_i - (L_i - L_j) * exp( - rT_B * (tL3((tL3(:,1) > tT_j),1) - tT_j)); % cm, struct length
-  ELw3 = [L_bj; L_jm]/ del_M;  % cm, total length
-  
-      %time-length 4
-  [t_j, t_p, t_b, l_j, l_p, l_b, l_i, rho_j, rho_B] = get_tj(pars_tj, f_34);
-  kT_M = k_M * TC_4; rT_j = rho_j * kT_M; rT_B = rho_B * kT_M;        
-  L_b = L_m * l_b;  L_j = L_m * l_j; L_i = L_m * l_i; tT_j = (t_j - t_b)/ kT_M;
-  L_bj = L_b * exp(tL4((tL4(:,1) <= tT_j),1) * rT_j/ 3); % cm, struc length
-  L_jm = L_i - (L_i - L_j) * exp( - rT_B * (tL4((tL4(:,1) > tT_j),1) - tT_j)); % cm, struct length
-  ELw4 = [L_bj; L_jm]/ del_M;  % cm, total length
+%     %time-length 3
+%   [t_j, t_p, t_b, l_j, l_p, l_b, l_i, rho_j, rho_B] = get_tj(pars_tj, f_34);
+%   kT_M = k_M * TC_3; rT_j = rho_j * kT_M; rT_B = rho_B * kT_M;        
+%   L_b = L_m * l_b;  L_j = L_m * l_j; L_i = L_m * l_i; tT_j = (t_j - t_b)/ kT_M;
+%   L_bj = L_b * exp(tL3((tL3(:,1) <= tT_j),1) * rT_j/ 3); % cm, struc length
+%   L_jm = L_i - (L_i - L_j) * exp( - rT_B * (tL3((tL3(:,1) > tT_j),1) - tT_j)); % cm, struct length
+%   ELw3 = [L_bj; L_jm]/ del_M;  % cm, total length
+%   
+%       %time-length 4
+%   [t_j, t_p, t_b, l_j, l_p, l_b, l_i, rho_j, rho_B] = get_tj(pars_tj, f_34);
+%   kT_M = k_M * TC_4; rT_j = rho_j * kT_M; rT_B = rho_B * kT_M;        
+%   L_b = L_m * l_b;  L_j = L_m * l_j; L_i = L_m * l_i; tT_j = (t_j - t_b)/ kT_M;
+%   L_bj = L_b * exp(tL4((tL4(:,1) <= tT_j),1) * rT_j/ 3); % cm, struc length
+%   L_jm = L_i - (L_i - L_j) * exp( - rT_B * (tL4((tL4(:,1) > tT_j),1) - tT_j)); % cm, struct length
+%   ELw4 = [L_bj; L_jm]/ del_M;  % cm, total length
 
-      % time-length A
+    %time length Castelo Branco
+  [t_j, t_p, t_b, l_j, l_p, l_b, l_i, rho_j, rho_B] = get_tj(pars_tj, f_CB);
+  kT_M = k_M * TC_CB; rT_j = rho_j * kT_M; rT_B = rho_B * kT_M;        
+  L_b = L_m * l_b;  L_j = L_m * l_j; L_i = L_m * l_i; tT_j = (t_j - t_b)/ kT_M;
+  L_bj = L_b * exp(tLCB(tLCB(:,1) < tT_j,1) * rT_j/ 3);
+  L_ji = L_i - (L_i - L_j) * exp( - rT_B * (tLCB(tLCB(:,1) >= tT_j,1) - tT_j)); % cm, expected length at time
+  Ww = [L_bj; L_ji].^3 * (1 + f_CB * w);
+  ELwCB = [L_bj; L_ji]/ del_M;  % cm, total length
+  
+    % time-length A
   [t_j, t_p, t_b, l_j, l_p, l_b, l_i, rho_j, rho_B] = get_tj(pars_tj, f_exp); %before experiment (no stress factor)
         %before experiment
   %kT_M = k_M * TC_bPA; rT_B = rho_B * kT_M; %no need
@@ -265,16 +307,6 @@ end
   Ww = L.^3 * (1 + f_E * w);
   ELwE = L / del_M; %cm, length
   EWwE = Ww; %g, wet weight
-  
-  %time wet weight Castelo Branco
-  [t_j, t_p, t_b, l_j, l_p, l_b, l_i, rho_j, rho_B] = get_tj(pars_tj, f_CB);
-  kT_M = k_M * TC_CB; rT_j = rho_j * kT_M; rT_B = rho_B * kT_M;        
-  L_b = L_m * l_b;  L_j = L_m * l_j; L_i = L_m * l_i; tT_j = (t_j - t_b)/ kT_M;
-  L_bj = L_b * exp(tWwCB(tWwCB(:,1) < tT_j,1) * rT_j/ 3);
-  L_ji = L_i - (L_i - L_j) * exp( - rT_B * (tWwCB(tWwCB(:,1) >= tT_j,1) - tT_j)); % cm, expected length at time
-  Ww = [L_bj; L_ji].^3 * (1 + f_CB * w);
-  ELwCB = [L_bj; L_ji]/ del_M;  % cm, total length
-  EWwCB = Ww; %g, wet weight
 
       %length-wet weight
   ELWw = (LWw(:,1)*del_M).^3 * (1 + f_exp * w); % g, wet weight
@@ -282,23 +314,190 @@ end
     %length-wet weight A
   ELWwE = (LWwE(:,1)*del_M).^3 * (1 + f_E * w); % g, wet weight
   
+  
+  %% Adriana %%
+  %time-length tL
+  
+  [tau_j, tau_p, tau_b, l_j, l_p, l_b, l_i, rho_j, rho_B, info] = get_tj(pars_tj, f_tL);
+  
+  %t-L RibeSara1999
+  %time-length since hatching concatenated to juveniles (from day 12)
+  kT_M = k_M * TC_tL;
+  rT_B = rho_B * kT_M;  % 1/d, von Bert growth rate   
+  rT_j = rho_j * kT_M;  % 1/d, exponential growth rate
+  tT_j = (tau_j - tau_b)/ kT_M; % time since *birth* at metamorphosis
+
+
+  L_j = l_j * L_m; 
+  L_i = l_i * L_m;
+  
+  EL_bj = L_b * exp(tL1((tL1(:,1)<= tT_j),1) * rT_j/3); % exponential growth as V1-morph
+%   EL_hj = L_h * exp (tL((tL(:,1)<= aT_j),1) * rT_j/3);
+  EL_ji = L_i - (L_i - L_j) * exp( - rT_B * (tL1((tL1(:,1) >= tT_j),1) - tT_j)); % cm, expected length at time
+  ELw1 = [EL_bj/del_Me; EL_ji/del_M]; % catenate lengths
+  
+  % tL2 RibeEngr2017 only juveniles
+  [tau_j, tau_p, tau_b, l_j, l_p, l_b, l_i, rho_j, rho_B, info] = get_tj(pars_tj, f_tL2);
+  kT_M2 = TC_tL2 * k_M;
+  rT_B =  rho_B * kT_M2;  % 1/d, von Bert growth rate   
+  rT_j =  rho_j * kT_M2;  % 1/d, exponential growth rate
+  tT_j = (tau_j - tau_b)/ kT_M2; % time since birth at metamorphosis
+%   aT_j = tau_j/ kT_M2 ; % time since *fertilization* at metamorphosis --> closer to hatching (which is where the dataset starts!)
+  
+  L_j = l_j * L_m; 
+  L_i = l_i * L_m;
+  EL_bj2 = Lw_b * exp(tL2((tL2(:,1)<= tT_j),1)  * rT_j/3); % exponential growth as V1-morph
+  EL_ji2 = L_i - (L_i - L_j) * exp( - rT_B * (tL2((tL2(:,1) > tT_j),1)- tT_j)); % cm, expected length at time
+  ELw2 = [EL_bj2; EL_ji2]/del_M; %TL2 is for juveniles all already metamorphosed
+  
+   %t-L3 TeixCabr2010 females
+  [tau_j, tau_p, tau_b, l_j, l_p, l_b, l_i, rho_j, rho_B, info] = get_tj(pars_tj, f_TeixCabr);
+  kT_M2 = TC_tL3 * k_M;
+  rT_B =  rho_B * kT_M2;  % 1/d, von Bert growth rate   
+  rT_j =  rho_j * kT_M2;  % 1/d, exponential growth rate
+  tT_j = (tau_j - tau_b)/ kT_M2; % time since birth at metamorphosis
+
+  L_j = l_j * L_m; 
+  L_i = l_i * L_m;
+  EL_bj3 = Lw_b * exp(tL3((tL3(:,1)<= tT_j),1)  * rT_j/3); % exponential growth as V1-morph
+  EL_ji3 = L_i - (L_i - L_j) * exp( - rT_B * (tL3((tL3(:,1) > tT_j),1)- tT_j)); % cm, expected length at time
+  ELw3 = [EL_bj3; EL_ji3]/del_M; 
+  
+   %t-L4 TeixCabr2010 males
+  [tau_j, tau_p, tau_b, l_j, l_p, l_b, l_i, rho_j, rho_B, info] = get_tj(pars_tj, f_TeixCabr2);
+  kT_M2 = TC_tL4 * k_M;
+  rT_B =  rho_B * kT_M2;  % 1/d, von Bert growth rate   
+  rT_j =  rho_j * kT_M2;  % 1/d, exponential growth rate
+  tT_j = (tau_j - tau_b)/ kT_M2; % time since birth at metamorphosis
+
+  L_j = l_j * L_m; 
+  L_i = l_i * L_m;
+  EL_bj4 = Lw_b * exp(tL4((tL4(:,1)<= tT_j),1)  * rT_j/3); % exponential growth as V1-morph
+  EL_ji4 = L_i - (L_i - L_j) * exp( - rT_B * (tL4((tL4(:,1) > tT_j),1)- tT_j)); % cm, expected length at time
+  ELw4 = [EL_bj4; EL_ji4]/del_M; %
+  
+  % length-weight
+ %lenght wet weight fish at 190, 398 and 790 days of age (manchado data)
+ELWwMan = (LWwMan(:,1) * del_M).^3 * (1 + f_field * w); 
+
+% ELWw2 = (LWw2(:,1) * del_M).^3 * (1 + f_Man * w); %for females
+% ELWw3 = (LWw3(:,1) * del_M).^3 * (1 + f_Man * w); %for males
+ 
+ 
+%Length dry weight --> laboratory conditions assume ab libitum use f=1
+%LWd (OrtiFune2019)
+L1 = LWd1(LWd1(:,1)<data.Lj,1) * del_Me; % for data before metamorphosis
+L2 = LWd1(LWd1(:,1)>=data.Lj,1) * del_Me; % for data after metamorphosis
+ELWd1 = [L1; L2].^3 * d_V*(1 + f * w)*1e6; % ug, wet weight 
+
+% here we assume that wga is the same before and after metamorphosis 
+
+%LWd2 (RibeEngr2017) --> they are all metamorphosed 
+%L3 = LWd2(LWd2(:,1)<data.Lj,1) * del_Me; %before metamorphosis
+L4 = LWd2(LWd2(:,1)>data.Lj,1) * del_Me; %after metamorphosis
+ELWd2 = L4.^3 * d_V* (1 + f * w)*1e6; % ug, dry weight 
+
+%LWd3 (YufeParr1999)
+L5 = LWd3(LWd3(:,1)<data.Lj,1) * del_Me; %before metamorphosis
+L6 = LWd3(LWd3(:,1)>data.Lj,1) * del_M; %after metamorphosis
+ELWd3 = [L5; L6].^3 * d_V* (1 + f * w)*1e6; % ug, dry weight 
+
+% time-dry weight
+% tWd (YufeParr1999)-->laboratory conditions assume ab libitum use f=1
+  [tau_j, tau_p, tau_b, l_j, l_p, l_b, l_i, rho_j, rho_B, info] = get_tj(pars_tj, f);
+  L_b = l_b * L_m; 
+  L_j = l_j * L_m; 
+  L_i = l_i * L_m;
+  
+    
+  tT_j1 = (tau_j - tau_b)/(k_M * TC_tWd);    % d, time since birth at metamorphosis corrected at 19 degrees for dry weight data
+  rT_j = rho_j * (k_M * TC_tWd);  
+  rT_B = rho_B * (k_M * TC_tWd);  
+  L_bj = L_b * exp(tWd((tWd(:,1) <= tT_j1),1) * rT_j/ 3);
+  L_jm = L_i - (L_i - L_j) * exp( - rT_B * (tWd((tWd(:,1) > tT_j1),1) - tT_j1)); % cm, expected length at time
+  EWd = [L_bj; L_jm].^3 * d_V * (1 + f * w) * 1e6 ;
+  
+ % tWd2 (ParrYufe2001) --> laboratory conditions assume ab libitum use f=1
+  tT_j1 = (tau_j - tau_b)/(k_M * TC_tWd2);    % d, time since birth at metamorphosis corrected at 19 degrees for dry weight data
+  rT_j = rho_j * (k_M * TC_tWd2);  
+  rT_B = rho_B * (k_M * TC_tWd2);  
+  L_bj = L_b * exp(tWd2((tWd2(:,1) <= tT_j1),1) * rT_j/ 3);
+  L_jm = L_i - (L_i - L_j) * exp( - rT_B * (tWd2((tWd2(:,1) > tT_j1),1) - tT_j1)); % cm, expected length at time
+  EWd2 = [L_bj; L_jm].^3 * d_V * (1 + f * w) * 1e6 ;
+  
+ %tWd_Feeding regimes (CañaFern1999)
+ %1 L100
+[tau_j, tau_p, tau_b, l_j, l_p, l_b, l_i, rho_j, rho_B] = get_tj(pars_tj, f_CanaFern1);
+rT_j = rho_j * (k_M * TC_tWd_f1); 
+rT_B = rho_B * (k_M * TC_tWd_f1); 
+tT_j = (tau_j - tau_b)/ (k_M * TC_tWd_f1); 
+L_bj = L_b * exp(tWd_f1(tWd_f1(:,1) < tT_j,1) * rT_j/ 3); % cm length and weight during V1-morph period
+L_jm = L_i - (L_i - L_j) * exp( - rT_B * (tWd_f1(tWd_f1(:,1) >= tT_j,1) - tT_j));   % cm, length after V1-morph period
+EWd_1 = 1e6 * [L_bj; L_jm].^3  * d_V * (1 + f_CanaFern1 * w); % ug, dry weight
+
+% %2 L50
+[tau_j, tau_p, tau_b, l_j, l_p, l_b, l_i, rho_j, rho_B] = get_tj(pars_tj, f_CanaFern2);
+rT_j = rho_j * (k_M * TC_tWd_f2); 
+rT_B = rho_B * (k_M * TC_tWd_f2); 
+tT_j = (tau_j - tau_b)/ (k_M * TC_tWd_f2); 
+L_bj = L_b * exp(tWd_f2(tWd_f2(:,1) < tT_j,1) * rT_j/ 3); % cm length and weight during V1-morph period
+L_jm = L_i - (L_i - L_j) * exp( - rT_B * (tWd_f2(tWd_f2(:,1) >= tT_j,1) - tT_j));   % cm, length after V1-morph period
+EWd_2 = 1e6 * [L_bj; L_jm].^3  * d_V * (1 + f_CanaFern2 * w); % ug, dry weight
+  
+%   
+% %3 L100I50
+[tau_j, tau_p, tau_b, l_j, l_p, l_b, l_i, rho_j, rho_B] = get_tj(pars_tj, f_CanaFern3);
+rT_j = rho_j * (k_M * TC_tWd_f3); 
+rT_B = rho_B * (k_M * TC_tWd_f3); 
+tT_j = (tau_j - tau_b)/ (k_M * TC_tWd_f3); 
+L_bj = L_b * exp(tWd_f3(tWd_f3(:,1) < tT_j,1) * rT_j/ 3); % cm length and weight during V1-morph period
+L_jm = L_i - (L_i - L_j) * exp( - rT_B * (tWd_f3(tWd_f3(:,1) >= tT_j,1) - tT_j));   % cm, length after V1-morph period
+EWd_3 = 1e6 * [L_bj; L_jm].^3  * d_V * (1 + f_CanaFern3 * w); % ug, dry weight
+  
+%4 L50I50
+[tau_j, tau_p, tau_b, l_j, l_p, l_b, l_i, rho_j, rho_B] = get_tj(pars_tj, f_CanaFern4);
+rT_j = rho_j * (k_M * TC_tWd_f4); 
+rT_B = rho_B * (k_M * TC_tWd_f4); 
+tT_j = (tau_j - tau_b)/ (k_M * TC_tWd_f4); 
+L_bj = L_b * exp(tWd_f4(tWd_f4(:,1) < tT_j,1) * rT_j/ 3); % cm length and weight during V1-morph period
+L_jm = L_i - (L_i - L_j) * exp( - rT_B * (tWd_f4(tWd_f4(:,1) >= tT_j,1) - tT_j));   % cm, length after V1-morph period
+EWd_4 = 1e6 * [L_bj; L_jm].^3  * d_V * (1 + f_CanaFern4 * w); % ug, dry weight
+  
+  
+  
   % pack to output
   %prdData.tLTeal = ELw;
+  prdData.tLCB = ELwCB;
   prdData.tLA = ELwA;
   prdData.tLB = ELwB;
   prdData.tLC = ELwC;
   prdData.tLD = ELwD;
   prdData.tLE = ELwE;
-  prdData.tL3 = ELw3;
-  prdData.tL4 = ELw4;
+%   prdData.tL3 = ELw3;
+%   prdData.tL4 = ELw4;
   prdData.tWwA = EWwA;
   prdData.tWwB = EWwB;
   prdData.tWwC = EWwC;
   prdData.tWwD = EWwD;
   prdData.tWwE = EWwE;
-  prdData.tWwCB = EWwCB;
   prdData.LWw = ELWw;
   prdData.LWwE = ELWwE;
+  
+  %Adriana
+  prdData.tL1 = ELw1;
+  prdData.tL2 =ELw2;
+  prdData.tL3 =ELw3;
+  prdData.tL4 =ELw4; 
+  prdData.LWwMan = ELWwMan;
+  prdData.LWd1 = ELWd1;
+  prdData.LWd2 = ELWd2;
+  prdData.LWd3 = ELWd3;
+  prdData.tWd = EWd;
+  prdData.tWd2 = EWd2;
+  prdData.tWd_f1 = EWd_1;
+  prdData.tWd_f2 = EWd_2;
+  prdData.tWd_f3 = EWd_3;
+  prdData.tWd_f4 = EWd_4;
   
   
 end
